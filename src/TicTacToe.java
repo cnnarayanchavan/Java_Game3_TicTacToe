75d4151bd565030972506a456317470beb87b07e
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-12 18:57:08 +0530

    Update TicTacToe.java_ changed color form pink to white at 72

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -68,7 +68,7 @@ public class TicTacToe extends JFrame {
 
         // adding the color into buttons tile and text on buttons 
         tile.setBackground(Color.darkGray);
-        tile.setForeground(Color.pink);
+        tile.setForeground(Color.white);
         tile.setFont(new Font("Arial", Font.BOLD, 120 ));
         
       }
