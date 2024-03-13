ed0de99d4f58d0a0755c4feec650a65e3e4b6ca0
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-13 09:31:36 +0530

    Update TicTacToe.java_SpeMis _ 001

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -71,7 +71,7 @@ public class TicTacToe extends JFrame {
         tile.setForeground(Color.red);
         tile.setFont(new Font("Arial", Font.BOLD, 120 ));
         tile.setFocusable(false);
-        tile.seText(cuuPlayer);
+        tile.setText(cuuPlayer);
         
         
       }
