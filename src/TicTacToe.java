280726d4d523496fcd2b2c539fdd95811e935397
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-15 13:00:25 +0530

    Update TicTacToe.java_modified

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -102,10 +102,8 @@ public class TicTacToe extends JFrame {
   {
     //Horizontal 
     for (int r = 0; r < 3; r++) {
-      if(board[r][0].getText()==" ") 
-      {
-          continue;
-      }
+      if(board[r][0].getText()==" ") continue;
+      
       if(board[r][0].getText()==board[r][1].getText()   &&
         board[r][1].getText()==board[r][2].getText())
       {
